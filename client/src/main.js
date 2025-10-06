e9a04757c09548325fbf82cd5ac4100f128d9b4a
Josue Ramirez <josue67ramirez@gmail.com>
2025-10-06 15:43:11 -0700

    initial commit

---
+++ client/src/main.js
@@ -0,0 +1,44 @@
+import "./style.css";
+
+const btnReq = document.querySelector(".btn");
+const image = document.getElementById("image");
+const prompt = document.getElementById("prompt");
+const emoji = document.querySelector(".emoji");
+
+//when the button is clicked a request is sent
+
+btnReq.addEventListener("click", async (e) => {
+  e.preventDefault();
+  if (prompt.length <= 0) return;
+  const message = prompt.value;
+
+  try {
+    startEmoji();
+
+    const fetchingImage = await fetch("http://localhost:3000/dream", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: message }),
+    });
+    const responseFromAi = await fetchingImage.json();
+    image.src = responseFromAi[0].url;
+    stopEmoji();
+  } catch (error) {
+    stopEmoji();
+    console.error(`Unable to fetch item: ${error}`);
+  }
+
+  prompt.value = "";
+});
+
+function startEmoji() {
+  emoji.classList.add("active");
+  btnReq.disabled = true;
+}
+
+function stopEmoji() {
+  emoji.classList.remove("active");
+  btnReq.disabled = false;
+}
---
+++ server/server.js
@@ -0,0 +1,38 @@
+import OpenAI from "openai";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+const port = 3000;
+
+//get will serve the html css side
+
+//post will initialized image generator
+
+app.post("/dream", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const responseFromAi = await openai.images.generate({
+      model: "dall-e-2",
+      prompt,
+      n: 1,
+      size: "512x512",
+    });
+    res.send(responseFromAi.data);
+  } catch (error) {
+    console.error(`Error generating image: ${error}`);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
