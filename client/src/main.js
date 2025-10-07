28ef1cd3806907fab6326ba6109968499d114c90
Josue Ramirez <josue67ramirez@gmail.com>
2025-10-07 10:22:57 -0700

    adding try/catch

--- client/src/main.js
+++ client/src/main.js
@@ -10,26 +10,31 @@ btnReq.addEventListener("click", async (e) => {
   if (prompt.length <= 0) return;
   const message = prompt.value;
   startEmoji();
-  const fetchingImage = await fetch(`https://dream2.onrender.com/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ prompt: message }),
-  });
 
-  if (fetchingImage.ok) {
+  try {
+    const fetchingImage = await fetch(`https://dream2.onrender.com/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: message }),
+    });
+
+    if (!fetchingImage.ok) {
+      const err = await fetchingImage.text();
+      alert(err);
+      console.error(err);
+    }
+
     const responseFromAi = await fetchingImage.json();
     image.src = responseFromAi[0].url;
-  } else {
-    const err = await fetchingImage.text;
-    alert(err);
-    console.error(err);
+  } catch (error) {
+    console.error("Fetching failed", error);
+    alert("Something went wrong. Please try again later");
+  } finally {
+    stopEmoji();
+    prompt.value = "";
   }
-
-  stopEmoji();
-
-  prompt.value = "";
 });
 
 function startEmoji() {
